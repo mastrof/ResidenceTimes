fb56a1875d714d8d3495300ed5c07300d26e62f7
Riccardo Foffi <rfoffi@ethz.ch>
2025-09-29 10:40:58 +0200

    add 30nM background chemoattractant

--- scripts/abm.jl
+++ scripts/abm.jl
@@ -13,19 +13,19 @@ dt = [0.1] # s
 U = [25, 45] # μm/s
 L = [1000] # μm
 Cs = [0, 0.1, 1, 5] # μM
-allparams = @strdict R mot dt U L Cs
+Cb = [0.03] # μM
+allparams = @strdict R mot dt U L Cs Cb
 dicts = dict_list(allparams)
 
 @everywhere function run_abm(config::Dict)
-    @unpack R, mot, dt, U, L, Cs = config
+    @unpack R, mot, dt, U, L, Cs, Cb = config
     γ = 150
-    model = setup_abm(; R, mot, dt, U, L, Cs, γ, n=50_000)
+    model = setup_abm(; R, mot, dt, U, L, Cs, Cb, γ, n=50_000)
     simtime = 60 # minutes
     nsteps = round(Int, simtime * 60 / dt)
     # radial distance from source
     r(a) = distance(a, chemoattractant(model).origin, model)
-    # c(a) = concentration(model)(position(a), model)
-    # relative concentration value at position
+    # normalized concentration value at position
     function _c(a)
         r = distance(a, chemoattractant(model).origin, model)
         exp(-(r-R)/γ) * R / r
