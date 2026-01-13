596231c369c7eae4c926c846eceb7428b42adbf2
Riccardo Foffi <rfoffi@ethz.ch>
2026-01-13 20:47:22 +0100

    community simulation

--- scripts/abm_community.jl
+++ scripts/abm_community.jl
@@ -22,25 +22,18 @@ dicts = dict_list(allparams)
 @everywhere function run_abm(config::Dict)
     @unpack mot, dt, U, λ, L, PER, Cb, α, Aphy = config
     γ = 150
-    model = setup_abm(; mot, dt, U, λ, L, α, Aphy, PER, Cb, γ, n=50_000)
+    model = setup_abm_community(; mot, dt, U, λ, L, α, Aphy, PER, Cb, γ, n=50)
     simtime = 75 # minutes
     nsteps = round(Int, simtime * 60 / dt)
-    # radial distance from source
-    function _c(a)
-        r = distance(a, chemoattractant(model).origin, model)
-        s = max(r, R)
-        exp(-(s-R)/γ) * R / s
-    end
-    c(a) = _c(a) # HACK: somehow necessary for correct naming
-    adata = [r, c]
-    # do not collect during first 30 minutes equilibration
-    # then collect every 45 seconds
-    when(model, t) = t >= 18_000 && t % 450 == 0
+    c(a) = concentration(model)(position(a), model)
+    adata = [c]
+    # do not collect during first 15 minutes equilibration
+    # then collect every 15 seconds
+    when(model, t) = t >= 0*6_000 && t % 150 == 0
     adf, = run!(model, nsteps; adata, when)
     # rename because local functions don't respect names
     colnames = names(adf)
     rename!(adf,
-        Symbol(colnames[end-1]) => :r,
         Symbol(colnames[end]) => :c,
     )
     adf
@@ -50,6 +43,6 @@ pmap(dicts) do config
     @show config
     data = run_abm(config)
     on_cluster = haskey(ENV, "SCRATCH")
-    fileout = datadir("abm", savename("abm", config, "csv"); on_cluster)
+    fileout = datadir("comm", savename("comm", config, "csv"); on_cluster)
     wsave(fileout, data)
 end
