040b17eac36dbbfc68092be8b7c47e94247ab714
Riccardo Foffi <rfoffi@ethz.ch>
2026-01-21 15:42:58 +0100

    update script

--- scripts/abm_community.jl
+++ scripts/abm_community.jl
@@ -7,26 +7,38 @@ using Distributed
     using MicrobeAgents
 end
 
-mot = ["RRF"]
-dt = [0.1] # s
-U = [10, 20, 40] # μm/s
-λ = [2.2] # 1/s
-L = [1000] # μm
-PER = [0.3, 0.5]
-Cb = [0.03] # μM
-α = [0.85]
-Aphy = Int.([3e5]) # cells/mL
-allparams = @strdict mot dt U λ L PER Cb α Aphy
-dicts = dict_list(allparams)
+@everywhere begin
+    # used for collecting data
+    x(a::AbstractMicrobe) = position(a)[1]
+    y(a::AbstractMicrobe) = position(a)[2]
+    z(a::AbstractMicrobe) = position(a)[3]
+end
+
+parameters = Dict(
+    :dt => 0.1, # s
+    :mot => "RRF",
+    :U => [10, 20, 40], # μm/s
+    :λ => 2.2, # 1/s
+    :PER => [0.3, 0.5],
+    :Cb => 0.0, # μM
+    :α => [0.75, 1.0],
+    :Aphy => Int.([3e4, 8e4, 3e5]), # cells/mL
+    :L => [ # μm
+        @onlyif(:Aphy == 3e4, 2150),
+        @onlyif(:Aphy == 8e4, 1550),
+        @onlyif(:Aphy == 3e5, 1000),
+    ],
+)
+dicts = dict_list(parameters)
 
 @everywhere function run_abm(config::Dict)
     @unpack mot, dt, U, λ, L, PER, Cb, α, Aphy = config
     γ = 150
-    model = setup_abm_community(; mot, dt, U, λ, L, α, Aphy, PER, Cb, γ, n=50_000)
-    simtime = 60 # minutes
+    model = setup_abm_community(; mot, dt, U, λ, L, α, Aphy, PER, Cb, γ, n=500_000)
+    simtime = 75 # minutes
     nsteps = round(Int, simtime * 60 / dt)
-    c(a) = concentration(model)(position(a), model) - Cb
-    adata = [c]
+    c(a) = concentration(model)(a, model) - Cb
+    adata = [x, y, z, c]
     # do not collect during first 15 minutes equilibration
     # then collect every 45 seconds
     eqtime = round(Int, 15*60 / dt)
